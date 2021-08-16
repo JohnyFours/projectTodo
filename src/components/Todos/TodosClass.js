42cecfd6da2175fb9e14cfeb71b92f6da67c738f
Aleksandr <aleks.protasov@outlook.com>
2021-08-16 17:21:20 +0300

    init

--- src/components/Todos/TodosClass.js
+++ src/components/Todos/TodosClass.js
@@ -1,62 +1,66 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class TodosClass extends Component {
 
-    state = {
-        newTodo:'',
-        todos:[],
+  state = {
+    newTodo: '',
+    todos: [],
+  }
+
+  addTodo = (e) => {
+    e.preventDefault();
+
+    const newTodoItem = {
+      id: Date.now(),
+      text: this.state.newTodo,
+      status: 'new',
     }
 
-    addTodo = (e) =>{
-        e.preventDefault();
-        const newTodoItem = {
-            id: Date.now(),
-            text: this.state.newTodo,
-            status: "new",
-        }
-        this.setState((prevState)=> (
-            {
-                newTodo:"",
-                todos:[newTodoItem, ...prevState.todos],
-            }
-            ))
-    }
+    this.setState((prevState) => (
+      {
+        newTodo: '',
+        todos: [newTodoItem, ...prevState.todos],
+      }
+    ))
+  }
 
-    inputChange = (event) =>{
-        this.setState({newTodo:event.target.value},()=>{
-            console.log(this.state.newTodo)
-        })
-    }
+  inputChange = (event) => {
+    this.setState({newTodo: event.target.value}, () => {
+      console.log(this.state.newTodo)
+    })
+  }
 
-    render() {
-        const {newTodo,todos} = this.state
-        return (
-            <div className="todos">
-                <form className="todos_form"
-                      onSubmit={this.addTodo}>
-                    <input
-                        type="text"
-                        placeholder='Your new todo...'
-                        value={newTodo}
-                        onChange={this.inputChange}
-                    />
-                    <button type="submit">Add todo</button>
-                </form>
-                <div className="todos__list">
-                    {todos.length
-                        ? (todos.map(({id, text}) => {
-                            return (
-                                <div className="todos__item"
-                                     key={id}>
-                                    {text}
-                                </div>
-                            );
-                        }))
-                        : <h2>No Todos...</h2>}
+  render () {
+    const {newTodo, todos} = this.state
+
+    return (
+      <div className="todos">
+        <form className="todos__form"
+              onSubmit={this.addTodo}>
+          <input
+            type="text"
+            value={newTodo}
+            onChange={this.inputChange}
+            placeholder="Your new todo..."
+          />
+          <button type="submit">Add todo</button>
+        </form>
+        <div className="todos__list">
+          {todos.length
+            ? (todos.map(({id, text: other}) => {
+              // const finalText = other + 1;
+              return (
+                <div className="todos__item"
+                     key={id}>
+                  {other}
                 </div>
-            </div>
-        );
-    }
+              );
+            }))
+            : <h2>No todos...</h2>}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default TodosClass;

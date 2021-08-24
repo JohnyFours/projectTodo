23efbd46ba0b7636ec382cc3a4f075d37db8dd44
Aleksandr <aleks.protasov@outlook.com>
2021-08-24 20:56:39 +0300

    init

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,7 +2,7 @@ const STATUSES = ['new', 'in progress', 'done', 'reject', 'deleted']
 const STATUSES2 = ['new', 'in prasdasdasdogress', 'done', 'reject', 'deleted']
 
 function f () {
-  // asdasd
+    // asdasd
 }
 
 export { STATUSES, STATUSES2, f }
